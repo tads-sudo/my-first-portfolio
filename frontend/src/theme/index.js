90f6fe7fc50f2001730673db569a06fc59349404
Mads97 <amadoviernes214@gmail.com>
2020-12-18 13:10:03 +0800

    Added colors and screen sizes.

---
+++ frontend/src/theme/index.js
@@ -0,0 +1,26 @@
+export const WHITE = "#ffffff";
+export const BLACK = "#000000";
+// COLORS
+export const SNOW_DRIFT = "#f9faf7";
+export const BURNING_ORANGE = "#ff7129";
+export const SUNSET_ORANGE = "#ff473a";
+export const TUATARA = "#2f2f2b";
+export const BANDICOOT = "#828276";
+export const CONIFER = "#71ea41";
+export const TURQUISE_BLUE = "#4bedea";
+export const color = {
+  PRIMARY: BURNING_ORANGE,
+  SECONDARY: TUATARA,
+  TERTIARY: SNOW_DRIFT,
+  SUBTLE: BANDICOOT,
+  SUCCESS: CONIFER,
+  ERROR: SUNSET_ORANGE,
+  LINKS: TURQUISE_BLUE,
+  BLACK: BLACK,
+  WHITE: WHITE,
+};
+
+//SCREEN SIZES
+export const SCREEN_MOBILE = { MIN: 320, MAX: 480 };
+export const SCREEN_TABLET = { MIN: 481, MAX: 1024 };
+export const SCREEN_DESKTOP = { MIN: 1025, MAX: 0 };
