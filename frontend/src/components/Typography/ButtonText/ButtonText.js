cb4929ea16aca8c2f3f1ea6dcb97ca23d113e2b6
Mads97 <amadoviernes214@gmail.com>
2020-12-19 01:11:01 +0800

    Created Button Text component.

---
+++ frontend/src/components/Typography/ButtonText/ButtonText.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useStyles } from "./style";
+
+export const ButtonText = ({ children }) => {
+  const classes = useStyles();
+  return <p className={classes.buttonText}>{children}</p>;
+};
+
+export default ButtonText;
