1e13f9a610aec6d28b45ee864e8d16d1292cbe1c
Mads97 <amadoviernes214@gmail.com>
2020-12-18 21:50:58 +0800

    Created subtitle component.

---
+++ frontend/src/components/Typography/Subtitle/Subtitle.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useStyles } from "./style";
+
+export const Subtitle = ({ children, bold = false }) => {
+  const classes = useStyles({ bold: bold });
+  return <p className={classes.subtitle}>{children}</p>;
+};
+
+export default Subtitle;
