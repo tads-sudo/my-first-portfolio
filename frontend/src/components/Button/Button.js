e6afa4a179ba0f210df9aca595167f08a90a5a36
Mads97 <amadoviernes214@gmail.com>
2020-12-26 11:29:22 +0800

    Created Button-Link module in button component.

--- frontend/src/components/Button/Button.js
+++ frontend/src/components/Button/Button.js
@@ -14,11 +14,36 @@ export const Button = ({
     sizeProps: size,
     cornerProps: corner,
     typeProps: type,
-    disabledProps: disabled,
   });
   return (
     <button className={classes.button} disabled={disabled}>
       {children}
     </button>
+  );
+};
+
+export const ButtonLink = ({
+  children,
+  href = "#",
+  color = "PRIMARY",
+  size = "LG",
+  corner = "0",
+  type = "fill",
+}) => {
+  const classes = useStyles({
+    colorProps: color,
+    sizeProps: size,
+    cornerProps: corner,
+    typeProps: type,
+  });
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noreferrer noopener"
+      className={classes.button}
+    >
+      {children}
+    </a>
   );
 };
