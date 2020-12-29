4248a68f836499632990a88508ab53683fa65925
Mads97 <amadoviernes214@gmail.com>
2020-12-29 21:08:21 +0800

    Deleted NavItem Text in typography component.

--- frontend/src/components/Typography/NavItemText/NavItemText.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useStyles } from "./style";
-
-export const NavItemText = ({ children }) => {
-  const classes = useStyles();
-  return <p className={classes.navItem}>{children}</p>;
-};
-
-export default NavItemText;
