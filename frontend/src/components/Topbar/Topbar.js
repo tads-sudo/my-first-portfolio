7f60a44039f705cdd5e49486a9f2bef9e464c27e
Mads97 <amadoviernes214@gmail.com>
2021-01-02 13:11:20 +0800

    Created top bar component.

---
+++ frontend/src/components/Topbar/Topbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { TopbarContainer } from "./TopbarContainer";
+import { Brand } from "./Brand";
+import { IconWrapper } from "./IconWrapper";
+import { CloseIcon } from "./CloseIcon";
+import { TopbarWrapper } from "./TopbarWrapper";
+import { TopbarMenu } from "./TopbarMenu";
+import { TopbarLink } from "./TopbarLink";
+
+export const Topbar = ({ toggle, isOpen }) => {
+  return (
+    <TopbarContainer isOpen={isOpen} onClick={toggle}>
+      <Brand onClick={toggle} />
+      <IconWrapper onClick={toggle}>
+        <CloseIcon />
+      </IconWrapper>
+      <TopbarWrapper>
+        <TopbarMenu>
+          <TopbarLink onClick={toggle} />
+        </TopbarMenu>
+      </TopbarWrapper>
+    </TopbarContainer>
+  );
+};
