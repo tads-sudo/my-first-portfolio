d8ae64ec514d31f83e76da40ecc8b51cea93f2c3
Mads97 <amadoviernes214@gmail.com>
2021-01-03 19:59:13 +0800

    Deleted placeholder text component.

--- frontend/src/components/Typography/PlaceholderText/PlaceholderText.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useStyles } from "./style";
-
-export const PlaceholderText = ({ children }) => {
-  const classes = useStyles();
-  return <p className={classes.placeholder}>{children}</p>;
-};
-
-export default PlaceholderText;
