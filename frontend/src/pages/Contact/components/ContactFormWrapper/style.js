de569f77b269136b232201f2e65e101046f5ae5f
Mads97 <amadoviernes214@gmail.com>
2021-01-03 20:30:58 +0800

    Created contact form wrapper component.

---
+++ frontend/src/pages/Contact/components/ContactFormWrapper/style.js
@@ -0,0 +1,9 @@
+import { createUseStyles } from "react-jss";
+
+export const useStyles = createUseStyles({
+  contactFormWrapper: {
+    padding: "30px",
+    background: "rgba(255, 255, 255, 0.03)",
+    borderRadius: "10px",
+  },
+});
