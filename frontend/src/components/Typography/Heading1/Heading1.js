9f022ab36bad0131870e49f523264a53105c748b
Mads97 <amadoviernes214@gmail.com>
2021-01-07 19:59:14 +0800

    Passed style props and added margin 0 property.

--- frontend/src/components/Typography/Heading1/Heading1.js
+++ frontend/src/components/Typography/Heading1/Heading1.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { useStyles } from "./style";
 
-export const Heading1 = ({ children }) => {
+export const Heading1 = ({ children, style }) => {
   const classes = useStyles();
-  return <h1 className={classes.heading}>{children}</h1>;
+  return (
+    <h1 className={classes.heading} style={style}>
+      {children}
+    </h1>
+  );
 };
