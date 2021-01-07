edf8ac7536ea63e0b27e197e9bad857ad220c1b0
Mads97 <amadoviernes214@gmail.com>
2021-01-07 19:48:18 +0800

    Deleted colored text component.

--- frontend/src/components/Typography/ColoredText/style.js
+++
@@ -1,7 +0,0 @@
-import { createUseStyles } from "react-jss";
-import { color } from "../../../theme";
-export const useStyles = createUseStyles({
-  text: (props) => ({
-    color: color[props.colorProps],
-  }),
-});
