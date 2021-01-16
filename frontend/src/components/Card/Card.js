d27abdfeed7a683768805c8d899857aa8ded841e
Mads97 <amadoviernes214@gmail.com>
2021-01-16 23:31:22 +0800

    Added style as props.

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { useStyles } from "./style";
-export const Card = ({ children }) => {
+export const Card = ({ children, style }) => {
   const classes = useStyles();
-  return <div className={classes.card}>{children}</div>;
+  return (
+    <div className={classes.card} style={style}>
+      {children}
+    </div>
+  );
 };
