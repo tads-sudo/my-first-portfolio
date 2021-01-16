a9dbb3ef4a18226333cfce0c4c4c3552e2c980d9
Mads97 <amadoviernes214@gmail.com>
2021-01-16 23:59:58 +0800

    Added style as props

--- frontend/src/components/Typography/Subtitle/Subtitle.js
+++ frontend/src/components/Typography/Subtitle/Subtitle.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { useStyles } from "./style";
 
-export const Subtitle = ({ children, bold = false }) => {
+export const Subtitle = ({ children, bold = false, style }) => {
   const classes = useStyles({ bold: bold });
-  return <p className={classes.subtitle}>{children}</p>;
+  return (
+    <p className={classes.subtitle} style={style}>
+      {children}
+    </p>
+  );
 };
