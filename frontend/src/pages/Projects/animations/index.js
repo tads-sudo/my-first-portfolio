b25e04ea6abdb27e808a06022a7820a2a548c54f
Mads97 <amadoviernes214@gmail.com>
2021-01-26 13:13:25 +0800

    Created project page animation.

---
+++ frontend/src/pages/Projects/animations/index.js
@@ -0,0 +1,6 @@
+export * from "./CategoriesAnimation";
+export * from "./FilterCardAnimation";
+export * from "./Heading3Animation";
+export * from "./FilterRow1Animation";
+export * from "./FooterAnimation";
+export * from "./SocialAnimation";
