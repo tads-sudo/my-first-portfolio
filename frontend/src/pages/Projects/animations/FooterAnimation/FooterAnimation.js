1ada05c046fbfeec705dbf88ac158f33c81d4389
Mads97 <amadoviernes214@gmail.com>
2021-01-27 17:47:19 +0800

    Edited transition delay  to 1 second

--- frontend/src/pages/Projects/animations/FooterAnimation/FooterAnimation.js
+++ frontend/src/pages/Projects/animations/FooterAnimation/FooterAnimation.js
@@ -7,7 +7,7 @@ export const FooterAnimation = ({ children, className }) => {
       className={className}
       initial={{ x: -20, opacity: 0 }}
       animate={{ x: 0, opacity: 1 }}
-      transition={{ delay: 0.8 }}
+      transition={{ delay: 1 }}
     >
       {children}
     </motion.div>
