b6a9a96c10b7a4a35d61208a9a52240960d603be
Mads97 <amadoviernes214@gmail.com>
2021-01-27 17:52:18 +0800

    Removed className props and edited the animation.

--- frontend/src/pages/Projects/animations/ProjectCardAnimation/ProjectCardAnimation.js
+++ frontend/src/pages/Projects/animations/ProjectCardAnimation/ProjectCardAnimation.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-export const ProjectCardAnimation = ({ children, className }) => {
+export const ProjectCardAnimation = ({ children }) => {
+  const card = {
+    hidden: {
+      scale: 0,
+      opacity: 0,
+    },
+    visible: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        delayChildren: 0.6,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
   return (
-    <motion.div
-      className={className}
-      initial={{ x: -20, opacity: 0 }}
-      animate={{ x: 0, opacity: 1 }}
-      transition={{ delay: 0.6 }}
-    >
+    <motion.div variants={card} initial="hidden" animate="visible">
       {children}
     </motion.div>
   );
