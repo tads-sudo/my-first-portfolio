f9e788b28d55a140830575cbb09f1722132cfd1a
Mads97 <amadoviernes214@gmail.com>
2021-02-01 15:16:41 +0800

    Deleted ContactFormWrapper component of contact page.

--- frontend/src/pages/Contact/components/ContactFormWrapper/ContactFormWrapper.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { useStyles } from "./style";
-
-export const ContactFormWrapper = ({ children }) => {
-  const classes = useStyles();
-  return <div className={classes.contactFormWrapper}>{children}</div>;
-};
--- frontend/src/pages/Contact/components/ContactFormWrapper/style.js
+++
@@ -1,9 +0,0 @@
-import { createUseStyles } from "react-jss";
-
-export const useStyles = createUseStyles({
-  contactFormWrapper: {
-    padding: "30px",
-    background: "rgba(255, 255, 255, 0.03)",
-    borderRadius: "10px",
-  },
-});
