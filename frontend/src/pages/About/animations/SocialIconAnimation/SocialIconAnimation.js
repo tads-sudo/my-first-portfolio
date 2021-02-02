af98d4ecd63348a4839e04472715407121ddc8c5
Mads97 <amadoviernes214@gmail.com>
2021-02-02 13:52:06 +0800

    Added variant entryAnimation.

--- frontend/src/pages/About/animations/SocialIconAnimation/SocialIconAnimation.js
+++ frontend/src/pages/About/animations/SocialIconAnimation/SocialIconAnimation.js
@@ -2,13 +2,24 @@ import React from "react";
 import { motion } from "framer-motion";
 
 export const SocialIconAnimation = ({ children, href }) => {
+  const entryAnimation = {
+    initial: {
+      opacity: 0,
+      y: -20,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: { type: "spring", damping: 5 },
+    },
+  };
+
   return (
     <motion.a
       href={href}
       target="_blank"
       rel="noreferrer noopener"
-      initial={{ y: -200, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
+      variants={entryAnimation}
       whileHover={{ scale: 1.2 }}
       whileTap={{ scale: 0.8 }}
     >
