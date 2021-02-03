76bf587fcce13d86339b2500907ac3d8ca01e5ea
Mads97 <amadoviernes214@gmail.com>
2021-02-03 12:45:28 +0800

    Imported PageAnimation

--- frontend/src/pages/Contact/ContactView.js
+++ frontend/src/pages/Contact/ContactView.js
@@ -1,36 +1,18 @@
 import React from "react";
-import { ContactForm, ContactFormWrapper } from "./components";
-import { Heading3, Text, LinkText } from "../../components";
-import { motion } from "framer-motion";
+import { useStyles } from "./style";
+import { ContactForm } from "./components";
+import { SocialIcons } from "./components";
+import { PageAnimation } from "./animations";
+
 export const ContactView = () => {
-  const pageAnimation = {
-    initial: {
-      opacity: 0,
-    },
-    in: {
-      opacity: 1,
-    },
-    out: {
-      opacity: 0,
-    },
-  };
+  const classes = useStyles();
 
   return (
-    <motion.section
-      initial="initial"
-      animate="in"
-      exit="out"
-      variants={pageAnimation}
-      // transition={{ type: "tween", ease: "anticipate" }}
-    >
-      <ContactFormWrapper>
-        <Heading3>Contact Me</Heading3>
-        <Text bold style={{ marginBottom: "24px" }}>
-          If you wanna talk about a project please fill in the form below or
-          send me an <LinkText>email</LinkText>.
-        </Text>
+    <PageAnimation className={classes.container}>
+      <div className={classes.contactForm}>
         <ContactForm />
-      </ContactFormWrapper>
-    </motion.section>
+      </div>
+      <SocialIcons />
+    </PageAnimation>
   );
 };
