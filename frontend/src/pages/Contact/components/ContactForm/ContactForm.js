15f261e09ce2462789edfde5d27700400bfe0721
Mads97 <amadoviernes214@gmail.com>
2021-02-14 22:05:22 +0800

    - Used useFormik to access errors in useEffect. - Used Object.keys method in useEffect to return if there is an array  of error keys. - If there is an array of error keys then it will focus the first index.

--- frontend/src/pages/Contact/components/ContactForm/ContactForm.js
+++ frontend/src/pages/Contact/components/ContactForm/ContactForm.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useStyles } from "./style";
 import { Icon } from "../../../../resources";
 import { ContactFormAnimation } from "../../animations";
 import { db } from "../../../../firebase";
-import { Formik } from "formik";
+import { useFormik } from "formik";
 import * as Yup from "yup";
 import { motion } from "framer-motion";
 import {
@@ -21,13 +21,6 @@ import {
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
 
-const initialState = {
-  name: "",
-  company: "",
-  email: "",
-  message: "",
-};
-
 const ContactSchema = Yup.object().shape({
   name: Yup.string().required("This field is required"),
   company: Yup.string().required("This field is required"),
@@ -41,24 +34,57 @@ export const ContactForm = () => {
   const classes = useStyles();
   const [successMessage, setSuccessMessage] = useState("");
   const [open, setOpen] = useState(false);
+  const {
+    values,
+    handleSubmit,
+    handleChange,
+    errors,
+    touched,
+    isSubmitting,
+  } = useFormik({
+    initialValues: {
+      name: "",
+      company: "",
+      email: "",
+      message: "",
+    },
+    onSubmit: (values, actions) => {
+      actions.setSubmitting(true);
 
-  const handleSubmit = (values, actions) => {
-    actions.setSubmitting(true);
+      setTimeout(() => {
+        db.collection("contacts")
+          .add(values)
+          .then(() => {
+            setSuccessMessage("Message has been submitted");
+            setOpen(true);
+            actions.resetForm();
+          })
+          .catch((error) => {
+            alert(error);
+          })
+          .finally(() => actions.setSubmitting(false));
+      }, 1200);
+    },
+    validationSchema: ContactSchema,
+    validateOnChange: false,
+  });
 
-    setTimeout(() => {
-      db.collection("contacts")
-        .add(values)
-        .then(() => {
-          setSuccessMessage("Message has been submitted");
-          setOpen(true);
-          actions.resetForm();
-        })
-        .catch((error) => {
-          alert(error);
-        })
-        .finally(() => actions.setSubmitting(false));
-    }, 1200);
+  const refs = {
+    name: useRef(null),
+    company: useRef(null),
+    email: useRef(null),
+    message: useRef(null),
+    send: useRef(null),
   };
+
+  useEffect(() => {
+    refs.name.current.focus();
+  }, []);
+
+  useEffect(() => {
+    const errorKeys = Object.keys(errors);
+    errorKeys.length > 0 && refs[errorKeys[0]].current.focus();
+  }, [errors]);
 
   const handleClose = (event, reason) => {
     if (reason === "clickaway") {
@@ -74,146 +100,134 @@ export const ContactForm = () => {
 
   return (
     <ContactFormAnimation className={classes.formWrapper}>
-      <Formik
-        initialValues={initialState}
-        onSubmit={handleSubmit}
-        validationSchema={ContactSchema}
-      >
-        {({
-          values,
-          handleSubmit,
-          handleChange,
-          errors,
-          touched,
-          isSubmitting,
-        }) => {
-          return (
-            <Form onSubmit={handleSubmit}>
-              <Heading3 style={{ marginBottom: "10px" }}>GET IN TOUCH</Heading3>
-              <Text style={{ marginBottom: "30px" }}>
-                Does your project need a Frontend Developer? Fill out this form
-                or let's get social.
-              </Text>
-              <FormRow>
-                <FormGroup>
-                  <FormInput
-                    label="Full Name"
-                    variant="outlined"
-                    fullWidth
-                    type="text"
-                    name="name"
-                    value={values.name}
-                    onChange={handleChange}
-                    error={touched.name && !!errors.name}
-                    helperText={touched.name && errors.name}
-                  />
-                </FormGroup>
+      <Form onSubmit={handleSubmit}>
+        <Heading3 style={{ marginBottom: "10px" }}>GET IN TOUCH</Heading3>
+        <Text style={{ marginBottom: "30px" }}>
+          Does your project need a Frontend Developer? Fill out this form or
+          let's get social.
+        </Text>
+        <FormRow>
+          <FormGroup>
+            <FormInput
+              label="Full Name"
+              variant="outlined"
+              fullWidth
+              type="text"
+              name="name"
+              value={values.name}
+              ref={refs.name}
+              onChange={handleChange}
+              error={touched.name && !!errors.name}
+              helperText={touched.name && errors.name}
+            />
+          </FormGroup>
 
-                <FormGroup>
-                  <FormInput
-                    label="Company"
-                    variant="outlined"
-                    fullWidth
-                    type="text"
-                    name="company"
-                    value={values.company}
-                    onChange={handleChange}
-                    error={touched.company && !!errors.company}
-                    helperText={touched.company && errors.company}
-                  />
-                </FormGroup>
-              </FormRow>
+          <FormGroup>
+            <FormInput
+              label="Company"
+              variant="outlined"
+              fullWidth
+              type="text"
+              name="company"
+              value={values.company}
+              ref={refs.company}
+              onChange={handleChange}
+              error={touched.company && !!errors.company}
+              helperText={touched.company && errors.company}
+            />
+          </FormGroup>
+        </FormRow>
 
-              <FormGroup>
-                <FormInput
-                  label="Email Address"
-                  variant="outlined"
-                  fullWidth
-                  type="email"
-                  name="email"
-                  value={values.email}
-                  onChange={handleChange}
-                  error={touched.email && !!errors.email}
-                  helperText={touched.email && errors.email}
-                />
-              </FormGroup>
+        <FormGroup>
+          <FormInput
+            label="Email Address"
+            variant="outlined"
+            fullWidth
+            type="email"
+            name="email"
+            value={values.email}
+            ref={refs.email}
+            onChange={handleChange}
+            error={touched.email && !!errors.email}
+            helperText={touched.email && errors.email}
+          />
+        </FormGroup>
 
-              <FormGroup>
-                <FormTextArea
-                  multiline
-                  label="Message"
-                  placeholder="Tell me about project details or anything"
-                  variant="outlined"
-                  rows={5}
-                  fullWidth
-                  name="message"
-                  value={values.message}
-                  onChange={handleChange}
-                  error={touched.message && !!errors.message}
-                  helperText={touched.message && errors.message}
-                />
-              </FormGroup>
+        <FormGroup>
+          <FormTextArea
+            multiline
+            label="Message"
+            placeholder="Tell me about project details or anything"
+            variant="outlined"
+            rows={5}
+            fullWidth
+            name="message"
+            value={values.message}
+            ref={refs.message}
+            onChange={handleChange}
+            error={touched.message && !!errors.message}
+            helperText={touched.message && errors.message}
+          />
+        </FormGroup>
 
-              <Button
-                disabled={isSubmitting}
-                buttonType="OUTLINE"
-                type="submit"
-                size="MD"
-                corner="8"
-                whileHover={{ scale: isSubmitting ? 1 : 1.05 }}
-                whileTap={{ scale: isSubmitting ? 1 : 0.85 }}
+        <Button
+          disabled={isSubmitting}
+          buttonType="OUTLINE"
+          type="submit"
+          size="MD"
+          corner="8"
+          ref={refs.send}
+          whileHover={{ scale: isSubmitting ? 1 : 1.05 }}
+          whileTap={{ scale: isSubmitting ? 1 : 0.85 }}
+        >
+          {isSubmitting ? (
+            <>
+              <motion.span
+                initial={{ scale: 0, y: 0, x: 0 }}
+                animate={{
+                  rotate: 40,
+                  y: [0, -100],
+                  x: [10, 0, 400],
+                  scale: [1, 1.5, 0],
+                }}
+                transition={{ duration: 1, ease: "easeInOut" }}
+                style={{ width: "30px", height: "24px" }}
               >
-                {isSubmitting ? (
-                  <>
-                    <motion.span
-                      initial={{ scale: 0, y: 0, x: 0 }}
-                      animate={{
-                        rotate: 40,
-                        y: [0, -100],
-                        x: [10, 0, 400],
-                        scale: [1, 1.5, 0],
-                      }}
-                      transition={{ duration: 1, ease: "easeInOut" }}
-                      style={{ width: "30px", height: "24px" }}
-                    >
-                      <SocialIcon style={{ marginRight: "6px" }}>
-                        <Icon.Send color="#000" />
-                      </SocialIcon>
-                    </motion.span>
-                    <Text bold>
-                      <TextProperty color="BLACK">Sending...</TextProperty>
-                    </Text>
-                  </>
-                ) : (
-                  <>
-                    <SocialIcon style={{ marginRight: "6px" }}>
-                      <Icon.Send color="#000" />
-                    </SocialIcon>
-                    <Text bold>
-                      <TextProperty color="BLACK">Send Message</TextProperty>
-                    </Text>
-                  </>
-                )}
-              </Button>
-              {successMessage && (
-                <Snackbar
-                  open={open}
-                  autoHideDuration={5000}
-                  onClose={handleClose}
-                  anchorOrigin={{
-                    vertical: "bottom",
-                    horizontal: "right",
-                  }}
-                >
-                  <Alert onClose={handleClose} severity="success">
-                    {successMessage}
-                  </Alert>
-                </Snackbar>
-              )}
-            </Form>
-          );
-        }}
-      </Formik>
+                <SocialIcon style={{ marginRight: "6px" }}>
+                  <Icon.Send color="#000" />
+                </SocialIcon>
+              </motion.span>
+              <Text bold>
+                <TextProperty color="BLACK">Sending...</TextProperty>
+              </Text>
+            </>
+          ) : (
+            <>
+              <SocialIcon style={{ marginRight: "6px" }}>
+                <Icon.Send color="#000" />
+              </SocialIcon>
+              <Text bold>
+                <TextProperty color="BLACK">Send Message</TextProperty>
+              </Text>
+            </>
+          )}
+        </Button>
+        {successMessage && (
+          <Snackbar
+            open={open}
+            autoHideDuration={5000}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+          >
+            <Alert onClose={handleClose} severity="success">
+              {successMessage}
+            </Alert>
+          </Snackbar>
+        )}
+      </Form>
     </ContactFormAnimation>
   );
 };
