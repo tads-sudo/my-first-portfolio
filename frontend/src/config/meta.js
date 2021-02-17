dbf68f2ba87fd13ff54ccb1390c9fafffabaf657
Mads97 <amadoviernes214@gmail.com>
2021-02-17 15:20:32 +0800

    Created config file for meta tags of pages.

---
+++ frontend/src/config/meta.js
@@ -0,0 +1,21 @@
+export const meta = {
+  ABOUT: {
+    TITLE: "Welcome to my portfolio | About Me",
+    DESCRIPTION:
+      "A personal web app to showcase my projects, skills, and experience.",
+  },
+  SKILLSEXPERIENCE: {
+    TITLE: "Skills and experience",
+    DESCRIPTION:
+      "A personal web app to showcase my projects, skills, and experience.",
+  },
+  PROJECTS: {
+    TITLE: "Side projects | Projects",
+    DESCRIPTION:
+      "A personal web app to showcase my projects, skills, and experience.",
+  },
+  CONTACT: {
+    TITLE: "Get in touch | Amado Viernes | Contact",
+    DESCRIPTION: "Fill out the form or let's get social.",
+  },
+};
