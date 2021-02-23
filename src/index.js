5485c1fa1b4b3bf684ea353815af9d1e2705656d
Mads97 <amadoviernes214@gmail.com>
2021-02-23 12:13:12 +0800

    Removed the preloader

--- src/index.js
+++ src/index.js
@@ -5,24 +5,15 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
-const loader = document.querySelector(".loader");
-
-const showLoader = () => loader.classList.remove("loader-hide");
-const hideLoader = () => loader.classList.add("loader-hide");
-
-setTimeout(
-  () =>
-    ReactDOM.render(
-      <React.StrictMode>
-        <Router>
-          <HelmetProvider>
-            <App hideLoader={hideLoader} showLoader={showLoader} />
-          </HelmetProvider>
-        </Router>
-      </React.StrictMode>,
-      document.getElementById("root")
-    ),
-  1000
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 reportWebVitals();
