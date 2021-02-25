79bf76b024061f50fb3a54ba60e93db17d4fe4c5
Mads97 <amadoviernes214@gmail.com>
2021-02-25 10:49:57 +0800

    Readded serviceWorker.js.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,5 +16,10 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
 
 reportWebVitals();
